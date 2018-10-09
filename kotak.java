97d14500973b106278c90f6eb99d160b22667c42
Nurdina <43734373+nurdinaDn@users.noreply.github.com>
2018-10-09 23:31:48 +0800

    Update kotak.java

--- kotak.java
+++ kotak.java
@@ -1,43 +1,30 @@
 package tugas;
 
+
 public class kotak {
 private double panjang, lebar, luas, keliling;
-	
-		//method setter
-		public void setpanjang(double panjang){
-			this.panjang = panjang;
-		}
-		
-		public void setlebar(double lebar){
-			this.lebar = lebar;
-		}
-		
-		public void setluas(double luas){
-			this.luas = luas;
-		}
-		
-		public void setkeliling(double keliling){
-			this.keliling = keliling;
-		}
-		
-		//method getter
-		public double getpanjang(){
-			return this.panjang;
-		}
-		
-		public double getlebar(){
-			return this.lebar;
-		}
-		
-		public double getluas(){
-			return this.luas;
-		}
-		
-		public double getkeliling(){
-			return this.keliling;
-		}
-		
-
-
+	void getPanjang() {
+		System.out.println(panjang);
+	}
+	void getLebar() {
+		System.out.println(lebar);
+	}
+	void getLuas() {
+		System.out.println(panjang * lebar);
+	}
+	void getKeliling() {
+		System.out.println(2 * (panjang + lebar));
+	}
+	void setPanjang() {
+		System.out.println(panjang);
+	}
+	void setLebar() {
+		System.out.println(lebar);
+	}
+	void setLuas() {
+		System.out.println(panjang * lebar);
+	}
+	void setKeliling() {
+		System.out.println(2 * (panjang + lebar));
+	}
 }
-
